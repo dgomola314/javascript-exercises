76e458eafeb386b2dd22e0344b2a100d930ca2ec
Danijel Gomola <180227226+dgomola314@users.noreply.github.com>
2024-09-16 22:29:44 +0200

    Create repeatString function
    This function repeats the string a given number of
    times.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let newString = string;
 
+    if (num > 0) {
+        for (i = 1; i < num; i++) {
+            newString += string;
+        }
+    
+        return newString;
+    } else if (num === 0) {
+        return "";
+    } else {
+        return "ERROR";
+    }
 };
 
 // Do not edit below this line
