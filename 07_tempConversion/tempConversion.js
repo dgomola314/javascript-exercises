efa10b76989c05460df0c01b09bd8b572a3292d9
Danijel Gomola <180227226+dgomola314@users.noreply.github.com>
2024-09-17 22:27:20 +0200

    Create function that does temperature conversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  const newTemp = (temp - 32) * (5 / 9);
+
+  return +newTemp.toFixed(1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  const newTemp = temp * (9 / 5) + 32;
+
+  return +newTemp.toFixed(1)
 };
 
 // Do not edit below this line
