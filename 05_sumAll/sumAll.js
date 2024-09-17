ed535f21b6028b4134daf671f7d95c276b530b58
Danijel Gomola <180227226+dgomola314@users.noreply.github.com>
2024-09-17 21:17:31 +0200

    Create sumAll function
    sumAll function takes two integers as input and
    returns a sum of all integers between
    (and including) them.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let sum = 0;
 
+    if (typeof num1 === "number" && typeof num2 === "number" && Number.isInteger(num1, num2) && num1 >=0 && num2 >= 0) {
+        if (num1 <= num2) {
+            for (i = num1; i <= num2; i++) {
+                sum += i;
+            }
+    
+            return sum;
+        } else if (num1 > num2) {
+            for (i = num1; i >= num2; i--) {
+                sum += i;
+            }
+    
+            return sum;
+        }
+    } else {
+        return "ERROR";
+    }
 };
 
 // Do not edit below this line
