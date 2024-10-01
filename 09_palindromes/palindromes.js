96cfde9fbe38260a4f5dee6f1ea8f0c3623b39fb
Danijel Gomola <180227226+dgomola314@users.noreply.github.com>
2024-10-01 21:10:15 +0200

    Create function that checks for palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    const alphanumerical = "qwertyuiopasdfghjklzxcvbnm0123456789";
 
+    const newString = string
+        .toLowerCase()
+        .split("")
+        .filter((char) => alphanumerical.includes(char))
+        .join("");
+
+    const reversedString = newString.split("").reverse().join("");
+
+    return reversedString === newString;
 };
 
 // Do not edit below this line
